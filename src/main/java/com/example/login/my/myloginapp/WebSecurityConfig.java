b6828bc489c67ba1b33001bc567c2a78bd586a00
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-04 08:23:18 +0900

    インメモリで認証をするように実装

--- src/main/java/com/example/login/my/myloginapp/WebSecurityConfig.java
+++ src/main/java/com/example/login/my/myloginapp/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.login.my.myloginapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
   
@@ -18,8 +20,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
           .and()
           // LOGIN
           .formLogin()
+          // /* */.loginPage("/login")
+          // /*    */.permitAll()
           /* */.defaultSuccessUrl("/success", true)
       // end
       ;
+  }
+
+  @Autowired
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+      auth
+          .inMemoryAuthentication()
+            .withUser("user").password("{noop}password").roles("USER");
   }
 }
