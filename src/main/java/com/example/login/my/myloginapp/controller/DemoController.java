f52868667e55b6dd6eb18b0eb28be0341c4ede00
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-05 12:53:15 +0900

    restcontrollerからcontrollerに変更

--- src/main/java/com/example/login/my/myloginapp/controller/DemoController.java
+++ src/main/java/com/example/login/my/myloginapp/controller/DemoController.java
@@ -2,27 +2,22 @@ package com.example.login.my.myloginapp.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 class DemoController {
   private static Log log = LogFactory.getLog(DemoController.class);
 
-   @GetMapping(path = "hello", produces = "text/html")
+   @GetMapping("/hello")
     public String hello() {
-        return "Hello!!!";
+        return "hello";
     }
 
-    @GetMapping(path = "success", produces = "text/html")
+    @GetMapping("/success")
     public String success() {
       log.info("success通過");
-        return "成功!!!";
+        return "success";
     }
 
-    @GetMapping(path = "page1", produces = "text/html")
-    public String page1() {
-      log.info("ページ1通過");
-        return "ページ１";
-    }
 }
